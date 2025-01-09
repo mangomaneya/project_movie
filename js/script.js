9b3eafec99d250f6f6e13ad08849b40bf0ba2d84
lee kiri <mango.man.e.ya@gmail.com>
2025-01-09 19:50:31 +0900

    flex를 사용하여 메인 ui작업 완료

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: "Bearer 631be9f8efc36b469ddab67aab82dd56"
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzFiZTlmOGVmYzM2YjQ2OWRkYWI2N2FhYjgyZGQ1NiIsIm5iZiI6MTczNjI5OTE2My44NDQsInN1YiI6IjY3N2RkMjliMDQ0YjZjYTY3NjRlNGYyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fnslzgWikJk9u6em-d-3k0O-2VENF4OeulMAtHTr-u8"
   }
 };
 const apiKey = "631be9f8efc36b469ddab67aab82dd56"; // 보안필요
@@ -15,26 +15,29 @@ let img_file_path = ""; // 이미지 파일 경로
 let logo_path = ""; // 로고파일 경로(넷플릭스 등) 
 
 // DOM 관련 선언영역
-const m_container = document.querySelector("#movies");
+const container_m = document.querySelector("#cont_movie");
+let movies = document.querySelectorAll(".movies")
+let movie_poster = document.querySelectorAll(".movie_poster")
+console.log("movie");
+// API데이터 읽어오기 테스트
+// fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+//   .then(res => res.json()) // json형식으로 응답 변환
+//   .then(res => console.log(res)) // 데이터를 콘솔에 출력
+//   .catch(err => console.error(err)); // 에러 처리
 
 fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
-  .then(res => res.json()) // json형식으로 응답 변환
-  .then(res => console.log(res)) // 데이터를 콘솔에 출력
-  .catch(err => console.error(err)); // 에러 처리
+  .then(res => res.json()) 
+  .then(res => {
+    
+    const movies = res.results;
+    container_m.innerHTML = movies.map((movie) => 
+      `<div class="movies">
+          <img alt="movie poster" class="movie_poster" src="${img_base_url}${movie.poster_path}">
+          <h2 class="movie_title">${movie.title}</h2>
+          <p class="movie_aver">⭐️ ${movie.vote_average}</p>
+      </div>`
+    ).join('');
+  })
+  .catch(err => console.error(err));
 
-// fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
-//   .then(res => res.json()) 
-//   .then(res => {
-//     const movies = res.results;
-//     m_container.innerHTML = movies.map((movie) => 
-//       `<div>
-//         <h2>${movie.title}</h2>
-//         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="size-500">
-//         <p>${movie.overview}</p>
-//       </div>`
-//     ).join('');
-//     console.log(movies);
-//   })
-//   .catch(err => console.error(err));
 
-// 
