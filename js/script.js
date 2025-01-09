7c890e2327fe69049efe0d0865aa62452daa6ae3
lee kiri <mango.man.e.ya@gmail.com>
2025-01-09 21:05:47 +0900

    modal창 구현중

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzFiZTlmOGVmYzM2YjQ2OWRkYWI2N2FhYjgyZGQ1NiIsIm5iZiI6MTczNjI5OTE2My44NDQsInN1YiI6IjY3N2RkMjliMDQ0YjZjYTY3NjRlNGYyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fnslzgWikJk9u6em-d-3k0O-2VENF4OeulMAtHTr-u8"
+    Authorization: "Bearer 631be9f8efc36b469ddab67aab82dd56"
+    // Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzFiZTlmOGVmYzM2YjQ2OWRkYWI2N2FhYjgyZGQ1NiIsIm5iZiI6MTczNjI5OTE2My44NDQsInN1YiI6IjY3N2RkMjliMDQ0YjZjYTY3NjRlNGYyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fnslzgWikJk9u6em-d-3k0O-2VENF4OeulMAtHTr-u8"
   }
 };
 const apiKey = "631be9f8efc36b469ddab67aab82dd56"; // 보안필요
@@ -17,18 +18,22 @@ let logo_path = ""; // 로고파일 경로(넷플릭스 등)
 // DOM 관련 선언영역
 const container_m = document.querySelector("#cont_movie");
 let movies = document.querySelectorAll(".movies")
-let movie_poster = document.querySelectorAll(".movie_poster")
-console.log("movie");
+let movie_poster = document.querySelectorAll(".movie_poster");
+const btn_search = document.querySelector("#search_submit");
+const input_search = document.querySelector("#input_search");
+let query = input_search.value.toLowerCase();
+
+console.log(movies);
 // API데이터 읽어오기 테스트
 // fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
 //   .then(res => res.json()) // json형식으로 응답 변환
 //   .then(res => console.log(res)) // 데이터를 콘솔에 출력
 //   .catch(err => console.error(err)); // 에러 처리
 
-fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+fetch(`${base_url}/movie/now_playing?language=en-US&page=1`, options)
   .then(res => res.json()) 
   .then(res => {
-    
+    console.log(res);
     const movies = res.results;
     container_m.innerHTML = movies.map((movie) => 
       `<div class="movies">
@@ -40,4 +45,14 @@ fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', op
   })
   .catch(err => console.error(err));
 
-
+// 검색 버튼 이벤트
+btn_search.addEventListener("click", () => {
+  alert(query);
+  // fetch(`${base_url}/search/movie?include_adult=false&language=en-US&page=1&query=${query}`,options)
+  // .then(res => res.json())
+  // .then(res => {
+  //   const result = res["results"];
+  //   console.log(result);
+  // })
+  // .catch(err => console.error(err))
+});
