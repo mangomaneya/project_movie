501b0f70bb675ee6eadf6f3c326f1d8e58d3f043
lee kiri <mango.man.e.ya@gmail.com>
2025-01-10 17:17:34 +0900

    minsu ing

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 
-export const fetchMovies = async function (url) { //url이라는 매개변수를 받는 fetchMovies라는 함수를 표현
+export const fetchMovies = async function (url) { //url이라는 매개변수를 받는 fetchMovies라는 함수를 표현 // export로 내보냄
     const options = { //api 호출 키 정보 변수
         method: 'GET',
         headers: {
@@ -10,7 +10,7 @@ export const fetchMovies = async function (url) { //url이라는 매개변수를
     //try catch문 : try 블록 내 코드가 먼저 실행되고, 만약 그 안에서 예외가 발생한다면 catch 블록 내 코드가 실행됩니다.
     try {
         const res = await fetch(url,options); //fetch : 네트워크에서 리소스를 취득하는 절차를 시작하고, 응답이 사용 가능해지면 이행하는 프로미스를 반환하는 메서드 
-        const { results } = await res.json();
+        const { results } = await res.json(); // 가져온 데이터를 json형식으로 가공 -> 구조분해할당!!!!!!으로 result값만 빠르게 추출....
         return results;
     }catch (err){ 
         console.error(`API 호출 에러 발생 - ${err}`);
--- js/script.js
+++ js/script.js
@@ -1,58 +1,60 @@
-// api 관련 선언영역
-const options = {
+//api.js에서 함수 import 
+import {fetchMovies} from "./api.js";
+
+const options = { //api 호출 키 정보 변수
   method: 'GET',
   headers: {
-    accept: 'application/json',
-    // Authorization: "Bearer 631be9f8efc36b469ddab67aab82dd56"
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzFiZTlmOGVmYzM2YjQ2OWRkYWI2N2FhYjgyZGQ1NiIsIm5iZiI6MTczNjI5OTE2My44NDQsInN1YiI6IjY3N2RkMjliMDQ0YjZjYTY3NjRlNGYyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fnslzgWikJk9u6em-d-3k0O-2VENF4OeulMAtHTr-u8"
-  }
+  accept: 'application/json',
+  Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzFiZTlmOGVmYzM2YjQ2OWRkYWI2N2FhYjgyZGQ1NiIsIm5iZiI6MTczNjI5OTE2My44NDQsInN1YiI6IjY3N2RkMjliMDQ0YjZjYTY3NjRlNGYyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fnslzgWikJk9u6em-d-3k0O-2VENF4OeulMAtHTr-u8"
+  },
 };
+
 const apiKey = "631be9f8efc36b469ddab67aab82dd56"; // 보안필요
-const base_url = "https://api.themoviedb.org/3"; //tmdb 기본 url
-const img_base_url ="https://image.tmdb.org/t/p/w500" // tmdb 이미지 소스 url
-const file_size = "original"; // 포스터 및 로고 이미지 부를 때 사이즈 설정
-// const file_type = "original"; // 로고 이미지 타입용이었는데 사이즈랑 중복되서 주석처리 
-let img_file_path = ""; // 이미지 파일 경로
-let logo_path = ""; // 로고파일 경로(넷플릭스 등) 
+const movie_url = "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1"; // 상영작 데이터 링크
+const img_base_url ="https://image.tmdb.org/t/p/original" // 이미지 소스 url 공통
+
 
 // DOM 관련 선언영역
 const container_m = document.querySelector("#cont_movie");
-let movies = document.querySelectorAll(".movies")
+// let movies = document.querySelectorAll(".movies")
 let movie_poster = document.querySelectorAll(".movie_poster");
 const btn_search = document.querySelector("#btn_search");
 const input_search = document.querySelector("#input_search");
 let query = input_search.value.toLowerCase();
+let movieData = [];
 
-// API데이터 읽어오기 테스트
-// fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
-//   .then(res => res.json()) // json형식으로 응답 변환
-//   .then(res => console.log(res)) // 데이터를 콘솔에 출력
-//   .catch(err => console.error(err)); // 에러 처리
+fetchMovies(movie_url).then(function (movies) {
+  makeMovieCard(movies);
+});
 
-//이건 동기로 처리된거... 
-fetch(`${base_url}/movie/now_playing?language=ko-KR&page=1`, options)
-  .then(res => res.json()) 
-  .then(res => {
-    console.log(res);
-    const movies = res.results;
-    container_m.innerHTML = movies.map((movie) => 
-      `<div class="movies">
-          <img alt="movie poster" class="movie_poster" src="${img_base_url}${movie.poster_path}">
+const makeMovieCard = function(movies){
+  movieData = movies;
+  container_m.innerHTML = movieData.map(function(movie) {
+    `<div class="movies" data-name =${movie.id}>
+          <img alt="movie poster" class="movie_poster" src="${img_base_url}${movies.poster_path}">
           <h2 class="movie_title">${movie.title}</h2>
           <p class="movie_aver">⭐️ ${movie.vote_average}</p>
       </div>`
-    ).join('');
-  })
-  .catch(err => console.error(err));
+  }).join('');
+};
+
+// 카드 ui뿌리기 
+// fetch(movie_url, options)
+//   .then(res => res.json()) 
+//   .then(res => {
+//     const movies = res.results;
+//     container_m.innerHTML = movies.map((movie) => 
+//       `<div class="movies">
+//           <img alt="movie poster" class="movie_poster" src="${img_base_url}${movie.poster_path}">
+//           <h2 class="movie_title">${movie.title}</h2>
+//           <p class="movie_aver">⭐️ ${movie.vote_average}</p>
+//       </div>`
+//     ).join('');
+//   })
+//   .catch(err => console.error(err));
 
 // 검색 버튼 이벤트
 btn_search.addEventListener("click", () => {
   alert(query);
-  // fetch(`${base_url}/search/movie?include_adult=false&language=en-US&page=1&query=${query}`,options)
-  // .then(res => res.json())
-  // .then(res => {
-  //   const result = res["results"];
-  //   console.log(result);
-  // })
-  // .catch(err => console.error(err))
+
 });
