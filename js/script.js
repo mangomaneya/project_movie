28c68ff19ba6a082e51fb50741b1922353494349
lee kiri <mango.man.e.ya@gmail.com>
2025-01-11 00:32:25 +0900

    js code 수정 및 검색기능 추가

--- js/script.js
+++ js/script.js
@@ -1,75 +1,81 @@
-//api.js에서 함수 import 
-import {fetchMovies} from "./api.js";
-
-const options = { //api 호출 키 정보 변수
-  method: 'GET',
+const options = {
+  method: "GET",
   headers: {
-  accept: 'application/json',
-  Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzFiZTlmOGVmYzM2YjQ2OWRkYWI2N2FhYjgyZGQ1NiIsIm5iZiI6MTczNjI5OTE2My44NDQsInN1YiI6IjY3N2RkMjliMDQ0YjZjYTY3NjRlNGYyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fnslzgWikJk9u6em-d-3k0O-2VENF4OeulMAtHTr-u8"
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzFiZTlmOGVmYzM2YjQ2OWRkYWI2N2FhYjgyZGQ1NiIsIm5iZiI6MTczNjI5OTE2My44NDQsInN1YiI6IjY3N2RkMjliMDQ0YjZjYTY3NjRlNGYyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fnslzgWikJk9u6em-d-3k0O-2VENF4OeulMAtHTr-u8",
   },
 };
+//api 관련 변수 선언
+let movieList = []; // 현재 상영중인 영화 목록 배열
+const movieUrl =
+  "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1";
+  const movieUrlSearch = "https://api.themoviedb.org/3/search/movie"
+  // ?query=${query}&include_adult=false&language=ko-KR&page=1
+const img_base_url ="https://image.tmdb.org/t/p/original";
 
-const apiKey = "631be9f8efc36b469ddab67aab82dd56"; // 보안필요
-const movie_url = "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1"; // 상영작 데이터 링크
-const img_base_url ="https://image.tmdb.org/t/p/original" // 이미지 소스 url 공통
-
-
-// DOM 관련 선언영역
-const container_m = document.querySelector("#cont_movie");
-// let movies = document.querySelectorAll(".movies")
-let movie_poster = document.querySelectorAll(".movie_poster");
+// DOM 관련 변수 선언
+const movieContainer = document.querySelector("#movie_container");
 const btn_search = document.querySelector("#btn_search");
 const input_search = document.querySelector("#input_search");
+const modal = document.querySelector(".modal");
+const btn_bookmark = document.querySelector(".bookmark");
 
-let movieData = [];
-
-fetchMovies(movie_url).then(function (movies) {
-  makeMovieCard(movies);
-});
-
-const makeMovieCard = function(movies){
-  movieData = movies;
-  container_m.innerHTML = movies.map(function(movie) {
-    return `<div class="movies" data-name =${movie.id}>
-          <img alt="movie poster" class="movie_poster" src="${img_base_url}${movie.poster_path}">
-          <h2 class="movie_title">${movie.title}</h2>
-          <p class="movie_aver">⭐️ ${movie.vote_average}</p>
-      </div>`
-  }).join('');
+// 현재 상영죽인 영화목록을 가져오기
+const fetchMovies = function (Url) {
+  return fetch(Url, options)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (res) {
+      return res.results;
+    });
 };
 
-// 검색 버튼 이벤트
-btn_search.addEventListener("click", () => {
-  let query = input_search.value.toLowerCase();
-  // alert(query);
- 
+//가져온 영화목록을 카드 ui로 만들기
+fetchMovies(movieUrl).then(function (result) {
+  makeMovieCard(result);
 });
 
-const search_movie = function(){
-  let movielistUrl = query>0 ? `https://api.themoviedb.org/3/search/movie?title=${query}&include_adult=false&language=ko&page=1` : movie_url ;
-  const q_movies = fetchMovies(movielistUrl);
-  container_m.innerHTML = "";
-  makeMovieCard(q_movies);
+// 영화정보로 카드 ui 만들기
+const makeMovieCard = function (movies) {
+  let tempHtml = "";
+  movies.forEach(function (m){
+    tempHtml = `<div class="movies">
+          <img alt="movie poster" class="movie_poster" src="${img_base_url}${m.poster_path}">
+          <h2 class="movie_title">${m.title}</h2>
+          <div class="movie_card_bott">
+            <p class="movie_aver">⭐️ ${m.vote_average}</p>
+          </div>
+        </div>`;
+    movieContainer.innerHTML += tempHtml;
+  });
+};
+
+//query가 포함된 검색결과를 카드에 반영하는 기능
+const searchMovie = function(query){
+  let searchUrl = query.length > 0 ? movieUrlSearch +`?query=${query}&include_adult=false&language=ko-KR&page=1` : movieUrl;
+  movieContainer.innerHTML = "";
+  fetchMovies(searchUrl).then(function (result) {
+    makeMovieCard(result);
+  }); 
 }
 
-// 카드 ui뿌리기 
-// fetch(movie_url, options)
-//   .then(res => res.json()) 
-//   .then(res => {
-//     const movies = res.results;
-//     container_m.innerHTML = movies.map((movie) => 
-//       `<div class="movies">
-//           <img alt="movie poster" class="movie_poster" src="${img_base_url}${movie.poster_path}">
-//           <h2 class="movie_title">${movie.title}</h2>
-//           <p class="movie_aver">⭐️ ${movie.vote_average}</p>
-//       </div>`
-//     ).join('');
-//   })
-//   .catch(err => console.error(err));
+// 버튼 클릭이벤트 - 검색실행
+btn_search.addEventListener("click", function(){
+  let query = input_search.value.toLowerCase();
+  searchMovie(query);
+})
+// 검색창 엔터 이벤트 - 검색실행
+function enterSearch(event){
+  let query = input_search.value.toLowerCase();
+  const code = event.code;
+  if (code === 'Enter'){
+    searchMovie(query);
+  }
+}
 
-
-
-fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+const toggleModal = function(){
+  modal.classList.toggle("hide");
+}
+btn_bookmark.addEventListener("click", toggleModal);
