e3855d3c9e0f840a8c9ed8daa7bf33d3f846d4b7
lee kiri <mango.man.e.ya@gmail.com>
2025-01-13 23:13:15 +0900

    모달 동작만 구현, 내용 진행중

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ let movieList = []; // 현재 상영중인 영화 목록 배열
 const movieUrl =
   "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1";
   const movieUrlSearch = "https://api.themoviedb.org/3/search/movie"
+  const movieDetailUrl = "https://api.themoviedb.org/3/movie/"
   // ?query=${query}&include_adult=false&language=ko-KR&page=1
 const img_base_url ="https://image.tmdb.org/t/p/original";
 
@@ -19,7 +20,10 @@ const movieContainer = document.querySelector("#movie_container");
 const btn_search = document.querySelector("#btn_search");
 const input_search = document.querySelector("#input_search");
 const modal = document.querySelector(".modal");
+const modal_close = document.querySelector(".detail_close_btn");
 const btn_bookmark = document.querySelector(".bookmark");
+const moviePoster = document.querySelector(".movie_poster")
+
 
 // 현재 상영죽인 영화목록을 가져오기
 const fetchMovies = function (Url) {
@@ -34,6 +38,7 @@ const fetchMovies = function (Url) {
 
 //가져온 영화목록을 카드 ui로 만들기
 fetchMovies(movieUrl).then(function (result) {
+  console.log(result);
   makeMovieCard(result);
 });
 
@@ -41,7 +46,7 @@ fetchMovies(movieUrl).then(function (result) {
 const makeMovieCard = function (movies) {
   let tempHtml = "";
   movies.forEach(function (m){
-    tempHtml = `<div class="movies">
+    tempHtml = `<div class="movies" data-id="${m.id}">
           <img alt="movie poster" class="movie_poster" src="${img_base_url}${m.poster_path}">
           <h2 class="movie_title">${m.title}</h2>
           <div class="movie_card_bott">
@@ -75,7 +80,50 @@ function enterSearch(event){
   }
 }
 
+//영화 상세창 관련 DOM가져오기
+let movie_poster_d = document.querySelector(".movie_poster_detail");
+let movie_title_d = document.querySelector(".movie_tltle_detail");
+let movie_desc_d = document.querySelector(".movie_desc");
+let movie_aver_d = document.querySelector(".movie_aver_detail");
+let movie_date_d = document.querySelector(".movie_date");
+
+//data-id 속성을 가지고 영화정보를 가져와서 모달에 반영하기 
+const movieDetail = function(id){
+  let idUrl = `${movieDetailUrl}${id}?language=ko-KR`;
+  fetchMovies(idUrl).then(function(result){
+    console.log(result);
+    makeMovieDetail(result);
+  });
+};
+
+// 영화 선택 클릭 이벤트 
+movieContainer.addEventListener("click",function(e){
+  let dataId = "";
+  if (e.target.closest(".movies") !== null){
+    dataId = e.target.closest(".movies").getAttribute("data-id");
+  }else{
+    return;
+  }
+  console.log(dataId);  
+  movieDetail(dataId);
+  toggleModal();
+})
+
+const makeMovieDetail = function(info){
+  movie_poster_d.textContent = info.title;
+  movie_poster_d.setAttribute("src",`${img_base_url}${info.poster_path}`);
+  movie_desc_d.textContent = info.overview;
+  movie_date_d.textContent =info.release_date;
+  movie_aver_d.textContent = info.vote_average;
+}
+
+// 모달 토글 기능
 const toggleModal = function(){
   modal.classList.toggle("hide");
 }
-btn_bookmark.addEventListener("click", toggleModal);
+// 닫기버튼 누르면 닫히기
+modal_close.addEventListener("click", toggleModal);
+//모달 바깥부분 누르면 닫히기
+window.addEventListener("click", function(e){
+  e.target === modal ? toggleModal() : false;
+})
