6d29d6e9938562955e8b77038678f76aa9d08223
lee kiri <mango.man.e.ya@gmail.com>
2025-01-15 16:33:25 +0900

    async/await적용

--- js/api.js
+++ js/api.js
@@ -9,24 +9,60 @@ export const options = {
 };
 
 // 현재 상영죽인 영화 리스트 fetch
-export const fetchMovies = function (Url) {
-  return fetch(Url, options)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (res) {
-      return res.results;
-    });
+// export const fetchMovies = function (Url) {
+//   return fetch(Url, options)
+//     .then(function (res) {
+//       return res.json();
+//     })
+//     .then(function (res) {
+//       return res.results;
+//     });
+// };
+
+//try catch문으로 바꿔본 fetchMovies
+export const fetchMovies = async (Url) => {
+  try {
+    const res = await fetch(Url, options);
+    const respones = await res.json();
+    return respones.results;
+  } catch (error) {
+    console.log(`에러가 발생했습니다. 에러내용 :${error}`);
+  }
 };
 
+//try catch문으로 바꿔본 fetchMovies 2번째
+// export const fetchMovies = async (Url) => {
+//   try {
+//     return await fetch(Url, options)
+//     .then((res)=>{
+//       return res.json();
+//     }).then((res)=>{
+//       return res.results;
+//     })
+//   } catch (error) {
+//     console.log(`에러가 발생했습니다. 에러내용 :${error}`);
+//   }
+// };
+
 // 영화 상세정보 fetch
-export const fetchMovies_detail = function (Url) {
-  return fetch(Url, options)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (res) {
-      // const {title, vote_average, release_date, poster_path,overview } =
-      return res;
-    });
-};
+// export const fetchMovies_detail = function (Url) {
+//   return fetch(Url, options)
+//     .then(function (res) {
+//       return res.json();
+//     })
+//     .then(function (res) {
+//       return res;
+//     });
+// };
+
+//try..catch문으로 바꿔본 fetchMovies_detail
+export const fetchMovies_detail = async (Url)=>{
+  try {
+    const response = await fetch(Url,options) 
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    console.log(`에러가 발생했습니다. 에러내용 :${error}`);
+  }
+}
+
